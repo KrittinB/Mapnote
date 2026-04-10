16af6f8bfc89b2011038a666486ce2ec7fa96c6b
KrittinB <krittinboonsu48@gmail.com>
2026-04-10 17:58:41 +0700

    update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,5 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.google.services) apply false
+    // alias(libs.plugins.google.services) apply false // เปิดใช้เมื่อเพิ่ม google-services.json แล้ว
 }
